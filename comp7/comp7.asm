fe249575696ecc2e09a81962d793ccc700a69111
Stuart Martin <stuart.martin@groupcdg.com>
2016-07-17 22:43:56 +0100

    atom build file + two chars in atttr

--- comp7/comp7.asm
+++ comp7/comp7.asm
@@ -25,17 +25,19 @@
 
 org 24000
 
-CHARS_START equ $3C00
+ROM_CHARS_START equ $3C00
 ATTRS_START equ $5800
 
 ; ROM char = 3C00 + 8 * character code (A=65) = $3C00 + 8x65 = $3E08
 
-; bright white is white ink 1x7 + white paper 8x7 + bright 64x1 = 127
+; bright white is white ink 1x7 + white paper 8x7 + bright 64x1 = 127d
 ; black is black ink 1x0 + black paper 8x0 = 0
+
+    ;; init screen
 
     ld a,0                      ; 0 is the code for black.
     out (254),a                 ; set border colour
-    ld bc,768                   ; prepare to write attrs for 32 chars x 22 rows
+    ld bc,768                   ; prepare to write attrs for 32 chars x 24 rows
     ld de,ATTRS_START           ; start of attrs displayfile
     push af                     ; protect a
 FILL_BLACK
@@ -47,86 +49,63 @@ FILL_BLACK
     ld a,b
     or c                        ; counter at 0?
     jp nz,FILL_BLACK            ; no continue
+    pop af                      ; balance stack
 
-PRINT_W
-    ld hl,(COMP_TEXT)           ; "W" = $57 = 87d
-    ld h,0                      ; zero high byte
+    ;; show scrolltext
+
+SCROLL
+    ld bc,0000
+    ld hl,ATTRS_START           ; hl := attr displayfile
+    ld (ATTR_LOC),HL
+DRAW_CHARS
+    ld hl,COMP_TEXT             ; start of comp text
+    add hl,bc                   ; move to next text char
+DRAW_CHAR
+    ld a,(hl)                   ; fetch character
+    ld l,a                      ; place value into hl ready for multiplication
+    ld h,0
     add hl,hl                   ; char x 2
     add hl,hl                   ; char x 4
     add hl,hl                   ; char x 8 ($57 * 8 = $2B8)
-    ld de,CHARS_START
+    ld de,ROM_CHARS_START
     add hl,de                   ; $3C00 + $2B8 = "W" in font rom
-    ld c,8                      ; process this many bytes (rows) per character
-    ld b,0                      ; zero high byte of bc
-    ex de,hl                    ; de := char row byte
-    ld hl,ATTRS_START           ; hl := attr displayfile
-DRAW_CHAR_ROW
-    ld a,(de)                   ; char row byte N
-    bit 7,a
-    jp z,DRAW_NO_BIT7
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT7
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 6,a
-    jp z,DRAW_NO_BIT6
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT6
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 5,a
-    jp z,DRAW_NO_BIT5
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT5
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 4,a
-    jp z,DRAW_NO_BIT4
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT4
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 3,a
-    jp z,DRAW_NO_BIT3
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT3
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 2,a
-    jp z,DRAW_NO_BIT2
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT2
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 1,a
-    jp z,DRAW_NO_BIT1
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT1
-    inc hl
-    ld a,(de)                   ; char row byte N
-    bit 0,a
-    jp z,DRAW_NO_BIT0
-    ld a,127
-    ld (hl),a                   ; draw attr
-DRAW_NO_BIT0
-    inc hl
     push bc
-    ld bc,24
+    ld bc,0008                  ; process this many bytes (rows) per character
+    ex de,hl                    ; de := char row byte, forget hl
+DRAW_CHAR_ROW
+    push bc
+    ld bc,0008                  ; process this many bits per byte per character row
+    ld a,(de)                   ; byte row for character
+DRAW_CHAR_BIT
+    rrca                        ; rotate bit0 into carry to inspect it
+    jp nc,DRAW_NO_BIT           ; was it a 1?
+    push af
+    ld a,127                    ; yes
+    ld hl,(ATTR_LOC)
+    ld (hl),a                   ; draw attr
+    pop af
+DRAW_NO_BIT
+    ld hl,(ATTR_LOC)
+    inc hl                      ; next attr space
+    ld (ATTR_LOC),hl
+    dec c
+    jp nz,DRAW_CHAR_BIT         ; done all bits in byte?
+    ld bc,24                    ; wrap to start of next attr row
+    ld hl,(ATTR_LOC)
     add hl,bc                   ; move attr displayfile on to next row
+    ld (ATTR_LOC),hl
     inc de                      ; next byte row for font character
     pop bc
     dec c                       ; done all byte rows for character?
     jp nz,DRAW_CHAR_ROW
-spin
-    jr spin
+    pop bc
+    inc c
+    ld a,c
+    cp 2
+    jp nz,DRAW_CHARS
     ret
+
+ATTR_LOC defb 0,0
 
 COMP_TEXT
     defb "Welcome to the Z80 Assembly Programming "
