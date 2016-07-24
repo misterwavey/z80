fbce860a0d1a5d1dfac9a2f9e3d1ef70cce0baa9
Stuart Martin <stuart.martin@groupcdg.com>
2016-07-24 16:35:27 +0100

    working

--- comp7/comp7.asm
+++ comp7/comp7.asm
@@ -1,126 +1,151 @@
-; ;;compo #7
-;
-;  Final size is the size of the code plus any initialised variables,
-;  you cannot assume RAM will be 0 though.
-; DETAILS: You must scroll the given scroll text across the screen using
-;  attributes rather than pixels.
-; 1) You may READ from the ROM Font located at $3d00 but no other ROM access
-;  is allowed. You cannot Read or Write to any other location below $4000.
-; 2) The scrolltext is included in the size of the entry, but you are free
-;  to encode it how you like, it must read correctly when run.
-; 3) The scrolltext must loop constantly
-; 4) You must include a halt (or similar) for timing. You are free to assume
-;  interrupts will be enabled.
-; 5) The text must be in bright white on black (with a black border)
-; 6) It must be on the top 8 attribute lines
-; 7) All entries MUST BE EMAILED TO compo@zxasm.net. DO NOT POST source
-;  code to the group (Or other websites) before the end of the compo else
-;  your entry will be disqualified.
-; 8) Your program can be anywhere you like in RAM
-; SCROLLTEXT:
-; Welcome to the Z80 Assembly Programming On The ZX
-; Spectrum Compo #7 ScrollText......
-;
-; DEADLINE: 9am GMT July 18th.
+;comp7v3
 
 org 24000
 
 ROM_CHARS_START equ $3C00
 ATTRS_START equ $5800
+BRIGHT_WHITE_INK_ON_BLACK equ 127
 
 ; ROM char = 3C00 + 8 * character code (A=65) = $3C00 + 8x65 = $3E08
 
 ; bright white is white ink 1x7 + white paper 8x7 + bright 64x1 = 127d
 ; black is black ink 1x0 + black paper 8x0 = 0
 
-    ;; init screen
+; set border
+; clear attrs
+; loop over chars in text
+;   find rom char
+;   loop over rotatesize 1..8 times
+;       loop over rom char bytes 1..8 times
+;           rotate-left rom char byte rotatesize times
+;           draw carry at attr
+;       scroll attrs
 
-    ld a,0                      ; 0 is the code for black.
-    out (254),a                 ; set border colour
-    ld bc,768                   ; prepare to write attrs for 32 chars x 24 rows
-    ld de,ATTRS_START           ; start of attrs displayfile
-    push af                     ; protect a
-FILL_BLACK
-    pop af                      ; restore a
-    ld (de),a                   ; write to attr
-    push af                     ; protect a
-    inc de                      ; next attr
-    dec bc                      ; dec counter
-    ld a,b
-    or c                        ; counter at 0?
-    jp nz,FILL_BLACK            ; no continue
-    pop af                      ; balance stack
+    ;; set border
 
-    ;; show scrolltext
+    ld   a,7                    ; black
+    out  ($fe),a                ; permanent border
 
-SCROLL
-    ld bc,0000
-    ld hl,ATTRS_START           ; hl := attr displayfile
-    ld (ATTR_LOC),HL
-DRAW_CHARS
-    ld hl,COMP_TEXT             ; start of comp text
-    add hl,bc                   ; move to next text char
-DRAW_CHAR
-    ld a,(hl)                   ; fetch character
-    ld l,a                      ; place value into hl ready for multiplication
-    ld h,0
-    add hl,hl                   ; char x 2
-    add hl,hl                   ; char x 4
-    add hl,hl                   ; char x 8 ($57 * 8 = $2B8)
-    ld de,ROM_CHARS_START
-    add hl,de                   ; $3C00 + $2B8 = "W" in font rom
-    push bc
-    ld bc,0008                  ; process this many bytes (rows) per character
-    ex de,hl                    ; de := char row byte, forget hl
-DRAW_CHAR_ROW
-    push bc
-    ld bc,0008                  ; process this many bits per byte per character row
-    ld a,(de)                   ; byte row for character
-DRAW_CHAR_BIT
-    rrca                        ; rotate bit0 into carry to inspect it
-    jp nc,DRAW_NO_BIT           ; was it a 1?
+    ;; clear all attrs to black
+
+    ld   hl,ATTRS_START         ; $5800
+    ld   bc,32*24               ; 32 columns x 24 rows
+CLEAR_ATTR
+    ld   (hl),0                 ; black is black ink 1x0 + black paper 8x0 = 0
+    inc  hl
+    dec  bc
+    ld   a,b                    ; 16 bit decrement ...
+    or   c                      ; ... test. z set when b & c both 0
+    jr   nz,CLEAR_ATTR          ; loop to set all attrs to 0
+
+MAIN
+    ld   hl,COMP_TEXT           ; start of text to scroll
+
+LOOP_OVER_CHARS_IN_TEXT
+    ld   a,(hl)                 ; at end of text?
+    cp   0
+    jr   z,MAIN                 ; yes, restart
+
+    push hl                     ; store text char position
+
+    ;; find ROM char using comp text value
+    ld   l,a
+    ld   h,0                    ; put char val in hl
+    add  hl,hl                  ; x2. Add reqs HL when adding another reg pair
+    add  hl,hl                  ; char value x 4
+    add  hl,hl                  ; char value x 8
+    ld   de,ROM_CHARS_START
+    add  hl,de                  ; ROM char = 3C00 + 8 * character code
+    ex   de,hl                  ; de := ROM char position
+    ld   (ROM_CHAR_POSITION),de
+
+    ld   a,1
+    ld   (ROTATESIZE),a         ; setup rotate counter
+
+LOOP_OVER_ROTATESIZE
+    ld   b,8                    ; rom char byte count
+    ld   hl,ATTRS_START-1     ; last attrs col top row
+    xor  a                      ; clear carry for later rotations
+
+LOOP_OVER_BYTES_IN_ROM_CHAR
+    ld   a,(ROTATESIZE)
+    ld   c,a                    ; local rotate counter
+    ld   a,(de)                 ; examine current rom char byte
+    push af                     ;
+
+ROTATE_C_TIMES
+    pop  af                     ; restore rotated a
+    rl   a                      ; rotate rom char byte left
     push af
-    ld a,127                    ; yes
-    ld hl,(ATTR_LOC)
-    ld (hl),a                   ; draw attr
-    pop af
-DRAW_NO_BIT
-    ld hl,(ATTR_LOC)
-    inc hl                      ; next attr space
-    ld (ATTR_LOC),hl
-    dec c
-    jp nz,DRAW_CHAR_BIT         ; done all bits in byte?
-    ld bc,24                    ; wrap to start of next attr row
-    ld hl,(ATTR_LOC)
-    add hl,bc                   ; move attr displayfile on to next row
-    ld (ATTR_LOC),hl
-    inc de                      ; next byte row for font character
-    pop bc
-    dec c                       ; done all byte rows for character?
-    jp nz,DRAW_CHAR_ROW
-    pop bc
-    inc c
-    ld a,c
-    cp 2
-    jp nz,DRAW_CHARS
-    ret
+    dec  c                      ; reduce local rotate counter
+    ld   a,c
+    cp   0
+    jp   nz,ROTATE_C_TIMES
+    pop  af
+    jr   nc,NO_ROW_CELL         ; did rotation result in carry being set?
+    ld   a,BRIGHT_WHITE_INK_ON_BLACK    ; yes so set cell ...
+    jp   DRAW_ROW_CELL
+NO_ROW_CELL
+    ld   a,0                    ; no so draw empty cell...
+DRAW_ROW_CELL
+    ;; calc next row of attr
+    push bc
+    ld   bc,32                  ; add 32 to attrs ..
+    add  hl,bc                  ; .. for next row
+    pop  bc
 
-ATTR_LOC defb 0,0
+    ld   (hl),a
 
+    inc  de
+    dec  b
+    ld   a,b
+    cp   0
+    jp   nz,LOOP_OVER_BYTES_IN_ROM_CHAR
+
+    halt
+    ;; scroll attrs
+    push de
+    push hl
+    ld   de,ATTRS_START
+    ld   hl,ATTRS_START+1
+    ld   bc,8*32                ; top 8 ATTR rows
+    ldir                        ; shift attrs left
+    pop  hl
+    pop  de
+
+    ld   a,(ROTATESIZE)         ; bump rotatesize
+    inc  a
+    ld   (ROTATESIZE),a
+    ld   de,(ROM_CHAR_POSITION)
+    cp   9                     ; unless we've done all 8
+    jp   nz,LOOP_OVER_ROTATESIZE
+
+    pop  hl
+    inc  hl                     ; next text char
+    jp   LOOP_OVER_CHARS_IN_TEXT
+
+ROTATESIZE
+    defb 0
+ROM_CHAR_POSITION
+    defb 0,0
 COMP_TEXT
     defb "Welcome to the Z80 Assembly Programming "
-    defb "On The ZX Spectrum Compo #7 ScrollText......"
+    defb "On The ZX Spectrum Compo #7 ScrollText.....",0
 
 end 24000
 
-; 1 5800
-; 2 5820
-; 3 5840
-; 4 5860
-; 5 5880
-; 6 58a0
-; 7 58c0
-; 8 58e0
+; ATTR DISPLAY FILE
+; 32 WIDE X 24 HIGH = 768 cells
+;
+; 1 5800 .. 581F
+; 2 5820 .. 583F
+; 3 5840 .. 585F
+; 4 5860 .. 587F
+; 5 5880 .. 589F
+; 6 58a0 .. 58bF
+; 7 58c0 .. 58dF
+; 8 58e0 .. 58fF
+
 ; 9 5900
 ;10 5920
 ;11 2940
